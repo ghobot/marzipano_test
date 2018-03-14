d84862faee305c66d077f7ce5166552163fd1a41
ghobot <greg@sciencelifeny.com>
2018-03-13 23:03:02 -0400

    first upload
    upload of files from marzipano utility

---
+++ app-files/data.js
@@ -0,0 +1,45 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-hdr_stereo_left",
+      "name": "hdr_stereo_left",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -1.9254468534986806,
+        "pitch": 0.2143478184175862,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -1.9254468534986806,
+          "pitch": 0.2143478184175862,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "innovation room test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
